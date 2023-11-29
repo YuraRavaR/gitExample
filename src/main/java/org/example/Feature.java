2c5590dd62bdca15a77a46a22e7968b56f2aa6be
Yura Kochubailo <yurii.kochubailo@chisw.com>
2023-11-29 17:49:26 +0100

    add feature2, feature3

--- src/main/java/org/example/Feature.java
+++ src/main/java/org/example/Feature.java
@@ -3,5 +3,7 @@ package org.example;
 public class Feature {
     public static void main(String[] args) {
         System.out.println("Hello world! Feature");
+        System.out.println("Hello world! Feature2");
+        System.out.println("Hello world! Feature3");
     }
 }
